aefc027717e97b5439c7bdaf469f944ce7f1731d
DIYDamian <33351875+DIYDamian@users.noreply.github.com>
2023-10-09 18:27:30 -0400

    idk

--- Assets/Scripts/Movement & Camera/MouseRotation.cs
+++ Assets/Scripts/Movement & Camera/MouseRotation.cs
@@ -12,30 +12,41 @@ public class MouseRotation : MonoBehaviour
     float xRotation;
     float yRotation;
 
+    public float delay = 0.1f;
+    [Range(0.0f, 90.0f)]public float maxYLook = 90f;
+
+    private float startTime;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
 
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Get Mouse Input
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
-
-        yRotation += mouseX;
-
-        xRotation -= mouseY;
-
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         //rotate the camera and orientation
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        if (Time.time > startTime + delay) {
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+            yRotation += mouseX;
+
+            xRotation -= mouseY;
+
+            xRotation = Mathf.Clamp(xRotation, -maxYLook, maxYLook);
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        } else {
+            xRotation = 0;
+            yRotation = 0;
+        }
 
     }
 }
