c6ff8482e5a0ba715205b13100e93dd658ac31ed
DIYDamian <33351875+DIYDamian@users.noreply.github.com>
2023-11-14 13:42:07 -0500

    jjjjjjjjjjjjjjjjjjjjjjjjjjjjjj

--- Assets/Scripts/Management/GameManager.cs
+++ Assets/Scripts/Management/GameManager.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    private Vector3 spawnPos;
+    private float spawnOrientation = 0;
 
     void Awake()
     {
