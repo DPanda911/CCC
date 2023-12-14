e7c5f0f951ea507924a2640f26b7df74e448a388
DIYDamian <33351875+DIYDamian@users.noreply.github.com>
2023-12-13 22:28:14 -0500

    hell yeah

--- Assets/Scripts/Management/Doesn't work/StatueCheck.cs
+++ Assets/Scripts/Management/Doesn't work/StatueCheck.cs
@@ -11,34 +11,52 @@ public class StatueCheck : MonoBehaviour
     public float posZ;
     Rigidbody rb;
 
+    Vector3 newPos;
+
+    [SerializeField] string statueTag;
+
+    GameManager gm;
+    AudioSource src;
+    [SerializeField] AudioClip soundToMake;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        newPos = new Vector3(posX, posY - 0.4f, posZ);
         
+        gm = GameManager.instance;
+
+        if (gm.CheckForMiscTag(statueTag)) {
+            statueRight = true;
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            transform.position = newPos;
+        }
+
+        src = gameObject.AddComponent<AudioSource>();
+        src.playOnAwake = false;
+        src.volume = 1f;
+        src.spatialBlend = 1f;
+        src.maxDistance = 250f;
+        src.clip = soundToMake;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (statueRight) {
+            transform.position = Vector3.Lerp(transform.position, newPos, 0.02f);
+        }
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Void")
+        if ((other.gameObject.tag == "Void") && (!statueRight))
         {
             statueRight = true;
-            transform.position = new Vector3(posX, posY, posZ);
             rb.constraints = RigidbodyConstraints.FreezeAll;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Void")
-        {
-            statueRight = false;
+            gm.NewMiscTag(statueTag);
+            src.Play();
         }
     }
 
