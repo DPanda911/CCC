d8a16600951b0b53fa29b2e03abaeb9f82185f36
DPanda911 <dnetmage91104@gmail.com>
2023-12-13 21:46:13 -0500

    ye

--- Assets/Scripts/Management/Doesn't work/StatueCheck.cs
+++ Assets/Scripts/Management/Doesn't work/StatueCheck.cs
@@ -6,12 +6,15 @@ public class StatueCheck : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool statueRight = false;
-
+    public float posX;
+    public float posY;
+    public float posZ;
     Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
@@ -26,6 +29,7 @@ public class StatueCheck : MonoBehaviour
         if (other.gameObject.tag == "Void")
         {
             statueRight = true;
+            transform.position = new Vector3(posX, posY, posZ);
             rb.constraints = RigidbodyConstraints.FreezeAll;
         }
     }
